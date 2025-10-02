5f5eb4af6ae3d86297aa512c1ba8c9fa6de4f412
fedeminatta <minattafederico@gmail.com>
2025-10-02 16:14:06 -0300

    tp final iniciado

---
+++ tp_final/src/reservas.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+// importo rutas cuando las tengamos
+// import { router as v1SalonesRutas } from './v1/rutas/salonesRutas.js';
+
+const app = express();
+
+app.use(express.json());
+
+// app.use('/api/v1/salones', v1SalonesRutas);
+
+process.loadEnvFile();
+
+app.listen(process.env.PUERTO, () => {
+	console.log(`Servidor iniciado en ${process.env.PUERTO}`);
+});
