61523ebadc6a1f2761dd3a51517aab973bf711c1
Mateo Acuña <matiu098@gmail.com>
2025-11-07 22:47:40 -0300

    validaciones rutas de usuarios

--- tp_final/src/rutas/usuariosRutas.js
+++ tp_final/src/rutas/usuariosRutas.js
@@ -1,19 +1,43 @@
 import express from 'express';
 import UsuariosControlador from '../controladores/usuariosControlador.js';
 import autorizarUsuarios from '../middlewares/autorizarUsuarios.js';
+import { check } from 'express-validator';
+import { validarCampos } from '../middlewares/validarCampos.js';
+
 
 const usuariosControlador = new UsuariosControlador();
 const router = express.Router();
 
 const ROL_ADMIN = [1];
-const ROL_ADMIN_Y_EMPLEADO = [1, 2];
-const ROL_TODOS = [1, 2, 3];
+
 
 
 router.get('/', autorizarUsuarios(ROL_ADMIN),usuariosControlador.buscarTodos);
 router.get('/:id', autorizarUsuarios(ROL_ADMIN),usuariosControlador.buscarPorId);
-router.post('/', autorizarUsuarios(ROL_ADMIN),usuariosControlador.agregar);
-router.put('/:id', autorizarUsuarios(ROL_ADMIN),usuariosControlador.editar);
+router.post('/',
+    autorizarUsuarios(ROL_ADMIN),
+    [
+        check('nombre', 'El nombre es requerido').not().isEmpty(),
+        check('apellido', 'El apellido es requerido').not().isEmpty(),
+        check('nombre_usuario','El nombre_usuario debe de ser un email valido').isEmail(),
+        check('contrasenia','La contraseña es requerida').not().isEmpty(),
+        check('tipo_usuario','El tipo_usuario es requerido y debe ser numérico').not().isEmpty().isInt(),
+        validarCampos
+    ],
+    usuariosControlador.agregar
+);
+
+router.put('/:id',
+    autorizarUsuarios(ROL_ADMIN),
+    [
+        check('nombre', 'El nombre es requerido').not().isEmpty(),
+        check('apellido', 'El apellido es requerido').not().isEmpty(),
+        check('nombre_usuario','El nombre_usuario debe de ser un email valido').isEmail(),
+        check('tipo_usuario','El tipo_usuario es requerido y debe ser numérico').not().isEmpty().isInt(),
+        validarCampos
+    ],
+    usuariosControlador.editar
+);
 router.delete('/:id', autorizarUsuarios(ROL_ADMIN),usuariosControlador.eliminar);
 
 export {router};
