c2a1d190b5e97140bbfa9681a3f09fde562a8c4e
fedeminatta <minattafederico@gmail.com>
2025-11-15 07:19:08 -0300

    fix null en reservas

--- tp_final/src/servicios/reservasServicio.js
+++ tp_final/src/servicios/reservasServicio.js
@@ -1,100 +1,89 @@
 import Reservas from '../db/reservas.js';
 import ReservasServicios from '../db/reservas_servicios.js';
-import Notificacion from '../servicios/notificacionesServicios.js'
+import Notificacion from '../servicios/notificacionesServicios.js';
 
 export default class ReservasServicio {
-    constructor(){
-        this.reserva = new Reservas;
-        this.reservas_servicios = new ReservasServicios()
-        this.notificacion = new Notificacion();
-    }
-    buscarTodos = () => {
-        return this.reserva.buscarTodos();
-    }
+	constructor() {
+		this.reserva = new Reservas();
+		this.reservas_servicios = new ReservasServicios();
+		this.notificacion = new Notificacion();
+	}
+	buscarTodos = () => {
+		return this.reserva.buscarTodos();
+	};
 
-    buscarPorId = (reserva_id) =>{
-        return this.reserva.buscarPorId(reserva_id);
-    }
+	buscarPorId = (reserva_id) => {
+		return this.reserva.buscarPorId(reserva_id);
+	};
 
+	agregar = async (reserva) => {
+		const {
+			fecha_reserva,
+			salon_id,
+			usuario_id,
+			turno_id,
+			foto_cumpleaniero,
+			tematica,
+			importe_salon,
+			importe_total,
+			servicios,
+		} = reserva;
 
+		const nuevaReserva = {
+			fecha_reserva,
+			salon_id,
+			usuario_id,
+			turno_id,
+			foto_cumpleaniero: foto_cumpleaniero || null,
+			tematica,
+			importe_salon: importe_salon ?? null,
+			importe_total: importe_total ?? null,
+		};
 
-    agregar = async (reserva) => {
-        const {
-            fecha_reserva,
-            salon_id,
-            usuario_id,
-            turno_id,
-            foto_cumpleaniero,
-            tematica,
-            importe_salon,
-            importe_total,
-            servicios} = reserva;
+		//solo creo la reserva
+		const resultado = await this.reserva.agregar(nuevaReserva);
 
-            const nuevaReserva = {
-                fecha_reserva,
-                salon_id,
-                usuario_id,
-                turno_id,
-                foto_cumpleaniero,
-                tematica,
-                importe_salon,
-                importe_total}
+		if (!resultado) {
+			return null;
+		}
 
-            //solo creo la reserva
-            const resultado = await this.reserva.agregar(nuevaReserva);
+		//creo las relaciones reservas_-servicios
+		const relacionOk = await this.reservas_servicios.agregar(
+			resultado.reserva_id,
+			servicios
+		);
+		console.log(relacionOk);
 
-            if(!resultado) {
-                return null;
-            }
-        
-        //creo las relaciones reservas_-servicios
-            const relacionOk = await this.reservas_servicios.agregar(resultado.reserva_id,servicios);
-            console.log(relacionOk);
+		if (!relacionOk) {
+			await this.reserva.eliminar(reserva.reserva_id);
+			return null;
+		}
+		//busco y envío los datos de notificación
+		const datosParaNotificacion = await this.reserva.datosParaNotificacion(
+			resultado.reserva_id
+		);
 
-            if(!relacionOk){
-                await this.reserva.eliminar(reserva.reserva_id);
-                return null;
-            }
-        //busco y envío los datos de notificación
-            const datosParaNotificacion = await this.reserva.datosParaNotificacion(resultado.reserva_id);
+		console.log(datosParaNotificacion);
 
-            console.log(datosParaNotificacion)
+		await this.notificacion.enviarCorreo(datosParaNotificacion);
+		return this.reserva.buscarPorId(resultado.reserva_id);
+	};
 
-            await this.notificacion.enviarCorreo(datosParaNotificacion);
-            return this.reserva.buscarPorId(resultado.reserva_id);
-        
-    }
+	editar = (reserva_id, datosReserva) => {
+		const existe = this.reserva.buscarPorId(reserva_id);
 
+		if (!existe) {
+			return null;
+		}
+		return this.reserva.editar(reserva_id, datosReserva);
+	};
 
+	eliminar = (reserva_id) => {
+		const existe = this.reserva.buscarPorId(reserva_id);
 
-    editar = (reserva_id,datosReserva) =>{
-        
-        const existe = this.reserva.buscarPorId(reserva_id);
-
-        if(!existe){
-            return null;
-        };
-        return this.reserva.editar(reserva_id,datosReserva);
-    }
-
-
-
-    eliminar = (reserva_id) => {
-
-        const existe = this.reserva.buscarPorId(reserva_id);
-
-        if(!existe){
-            return null;
-        };
-        return this.reserva.eliminar(reserva_id);
-    }
-    }
-    
-
-
-
-
-
-
-
-
+		if (!existe) {
+			return null;
+		}
+		return this.reserva.eliminar(reserva_id);
+	};
+}
